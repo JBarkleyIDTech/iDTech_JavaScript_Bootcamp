821038566c234842c29abd99aaac5d68297159b5
JBarkleyIDTech <jobarkley@idtech.com>
2021-12-05 12:17:38 -0600

    Added the variables.js lesson to the repository

---
+++ variables.js
@@ -0,0 +1,14 @@
+var number_of_students = 30;
+console.log(number_of_students);
+
+number_of_students = 20;
+console.log(number_of_students);
+
+function studentName() {
+    let myName = "Joseph";
+    console.log(myName);
+}
+studentName();
+
+const year = 2021;
+console.log(year);
