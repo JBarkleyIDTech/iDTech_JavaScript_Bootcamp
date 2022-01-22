1abdb45aeb63b99b4b65a5ad923c7febe4fa9290
JoeBarkley <50744898+JoeBarkley@users.noreply.github.com>
2022-01-22 00:30:46 -0600

    Added the section 1 folder to my repo

---
+++ Section 1/types.js
@@ -0,0 +1,12 @@
+let myTestScore = 97.5;
+console.log(myTestScore);
+
+let favoriteFood = "Pizza";
+console.log(favoriteFood);
+
+let statement = "I like ";
+console.log(statement + favoriteFood);
+
+let weekend = false;
+let weekday = true;
+console.log(weekend);
---
+++ Section 1/variables.js
@@ -0,0 +1,15 @@
+var number_of_students = 5;
+console.log(number_of_students);
+
+number_of_students = 6;
+console.log(number_of_students);
+
+function studentName() {
+    let myName = "Joseph";
+    console.log(myName);
+}
+
+studentName();
+
+const year = 2021;
+console.log(year);
